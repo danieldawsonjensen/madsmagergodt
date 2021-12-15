bff49ca2e0e8271f51f973519accf94ac5792480
Daniel <dani540c@gmail.com>
2021-12-15 11:09:43 +0100

    ahmad

---
+++ public/main.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const { Client } = require("pg")
+
+const app = express()
+const api_port = 3000
+const klient = new Client({
+    user: "gfyahyvo",
+    host: "hattie.db.elephantsql.com",
+    database: "gfyahyvo",
+    password: "O_FjKVEHwWbqCJn3Rg7BW8YxtGl-JCAq",
+    port: 5432
+});
+
+const qry = `select * from final
+order by mad_id`
+
+app.get('/final', async (req, res) => {
+    try {
+        let queryData = await klient.query(qry);
+        res.json({
+            "ok": true,
+            "final": queryData.rows,
+        })
+    }
+    catch (error) {
+        res.json({
+            "ok": false,
+            "message": error.message,
+        })
+    }
+})
+
+klient.connect();
+
+app.use(express.static("public"))
+
+app.listen(api_port, () => {
+    console.log(`Appl. lytter på http://localhost:${api_port}`)
+})
