cb4c767994a392a278687dfab65e405f9b22dd86
danieldawsonjensen <dani540c@gmail.com>
2021-12-16 08:42:30 +0100

    ahmad

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ const express = require('express')
 const { Client } = require("pg")
 
 const app = express()
-const api_port = 3000
+
+const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.DB_USER || "my_user";
+const DB_HOST = process.env.DB_HOST || "my_host";
+const DB_NAME = process.env.DB_NAME || "database";
+const DB_PW = process.env.DB_PW || "password";
+const DB_PORT = process.env.DB_PORT || 5432;
+
+
 const klient = new Client({
-    user: "gfyahyvo",
-    host: "hattie.db.elephantsql.com",
-    database: "gfyahyvo",
-    password: "O_FjKVEHwWbqCJn3Rg7BW8YxtGl-JCAq",
-    port: 5432
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PW,
+    port: DB_PORT
 });
 
 const qry = `select * from final
